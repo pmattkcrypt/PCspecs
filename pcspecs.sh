1347c1ffbcb860ff8990354fe2d160e9a413cc75
Athanasios Makrovasilis <164661371+pmattkcrypt@users.noreply.github.com>
2026-02-02 22:51:51 +0200

    Update pcspecs.sh

--- pcspecs.sh
+++ pcspecs.sh
@@ -5,7 +5,7 @@
 # O/S:ubuntu LTS
 clear
 echo "The Shell Script gives basic computer's specifications"
-echo "*******************************************************"
+echo "******************************************************"
 name=$(hostname | awk '{print $1}')
 echo "C O M P U T E R  N A M E" > PCspecs_$name
 hostname >> PCspecs_$name
@@ -20,9 +20,12 @@ echo "M O T H E R B O A R D" >> PCspecs_$name
 sudo dmidecode -t 1 | grep Manufacturer >> PCspecs_$name
 sudo dmidecode -t 1 | grep Product >> PCspecs_$name
 echo "      " >> PCspecs_$name
+echo "V G A " >> PCspecs_$name
+lspci | grep -i vga >> PCspecs_$name;
+echo "      " >> PCspecs_$name
 echo "H D D " >> PCspecs_$name
-# gives the size, the model and the serial number of 2 hard disks on the system. Change the value of the tail command (tail -2) for more disks (example tail -4) 
-lsblk -d -o size,model,serial | tail -2 >> PCspecs_$name; 
+#gives the model and the serial number of 2 disks/devices on the system. Change the value of the tail command (tail -2) for more disks/devices (example tail -4) 
+lsblk -d -o model,serial | tail -2 >> PCspecs_$name; 
 echo "      " >> PCspecs_$name
 echo "O / S " >> PCspecs_$name
 lsb_release -a | tail -3 >> PCspecs_$name
